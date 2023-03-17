ea2fd9a7664fddf63e06134db45f2deb26396f52
Yun-Chi Hsiao <is0.jimhsiao@gmail.com>
2023-03-17 17:40:21 +1100

    Add:  Analyse functions

--- main.py
+++ main.py
@@ -1,29 +1,33 @@
 import json
+import tweet
+import utility
 
 def main():
 	print("Let's GO!!!")
 	print("\n")
 
 	# Opening JSON file
-	f = open('tinyTwitter.json')
-	data = json.load(f)
-	
+	tt = open('tinyTwitter.json')
+	sal = open('sal.json')
+
+	tt_data = json.load(tt)
+	sal_data = json.load(sal)
+
+	sal_dict = utility.create_sal_dict(sal_data)
 	# City dict
-	# city_count = {
-	# 	'syd': 0, 
-	# 	'mel': 0, 
-	# 	'bri': 0,
-	# 	'ade': 0,
-	# 	'per': 0,
-	# 	'hob': 0}
+	gcc_count = {'1gsyd': 0, '2gmel': 0, '3gbri': 0, '4gade': 0, 
+				'5gper': 0, '6ghob': 0, '7gdar': 0, '8acte': 0 }
+
+	user_record = {}
 
 	# Iterating through the json
-	# list
-	for i in data:
-		print(i['includes']['places'][0]['full_name'])
+	for i in tt_data:
+		# multi-threading...
+		tweet.analyse(i, user_record, gcc_count, sal_dict)
 	
 	# Closing file
-	f.close()
+	tt.close()
+	sal.close()
 	print("\n")
 	print("DONE!!!")
 
--- tweet.py
+++ tweet.py
@@ -1,9 +1,84 @@
 # Analyse a single tweet
-def analyse_tweet(tweet, user_record):
+def analyse(tweet, user_record, gcc_count, sal_data) -> None:
+	
 	user_id = tweet['data']['author_id']
+
+	# If user has been recorded
 	if user_id in user_record:
-		user_record[user_id]['tweet_count'] += 1
-		print("!!!")
+		user = user_record[user_id]
+		user['tweet_count'] += 1
 	else:
-		user_record[user_id] = {}
-		user_record[user_id]['tweet_count'] = 1
+		user = create_user()
+		user_record[user_id] = user
+	
+	# Handle reading location
+	location = tweet['includes']['places'][0]['full_name']
+	location = read_loc(location, sal_data)
+	
+	# Add a new great capital city
+	if len(location) != 0:
+		if user[location] == 0:
+			user['unique_city'] += 1
+		user[location] += 1
+		gcc_count[location] += 1
+
+	return
+
+# Read the location
+def read_loc(location, sal_dict) -> str:
+
+	gcc = {
+		'New South Wales': ('(nsw)', '1gsyd'),
+		'Victoria': ('(vic.)', '2gmel'),
+		'Queensland': ('(qld)', '3gbri'),
+		'South Australia': ('(sa)', '4gade'),
+		'Western Australia': ('(wa)', '5gper'),
+		'Tasmania': ('(tas.)', '6ghob'),
+		'Northern Territory': ('(nt)', '7gdar') ,
+		'Australian Capital Territory': ('(act)', '8acte')
+	}
+
+	city = {
+		'Sydney': '1gsyd',
+		'Melbourne': '2gmel',
+		'Brisbane': '3gbri',
+		'Adelaide': '4gade',
+		'Perth': '5gper',
+		'Tasmania': '6ghob',
+		'Darwin': '7gdar',
+	}
+
+	loc = location.split(', ')
+
+	if len(loc) < 2:
+		location = ""
+
+	elif loc[1] in city:
+		location = city[loc[1]]
+	elif loc[1] in gcc:
+		# check the loc and loc + state
+		if loc[0].lower() in sal_dict or loc[0]+" "+gcc[loc[1]][0].lower() in sal_dict:
+			location = gcc[loc[1]][1]
+		else:
+			location = ""
+	else:
+		location = ""
+
+	return location
+
+# Create new user
+def create_user() -> {}:
+	user = {
+		'tweet_count': 1,
+		'unique_city': 0,
+		'1gsyd': 0, 
+		'2gmel': 0, 
+		'3gbri': 0,
+		'4gade': 0,
+		'5gper': 0,
+		'6ghob': 0,
+		'7gdar': 0,
+		'8acte': 0
+	}
+
+	return user
