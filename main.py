757a4c0d54634df70e8df5f149c02729899aeeed
Yun-Chi Hsiao <is0.jimhsiao@gmail.com>
2023-04-08 12:33:48 +1000

    Update: distribution

--- main.py
+++ main.py
@@ -17,37 +17,30 @@ def main():
 	if rank == 0:
 		master_tweet_processor(comm, sal_dict, data_url)
 	else:
-		slave_tweet_processor(comm, sal_dict)
+		slave_tweet_processor(comm, sal_dict, data_url)
 	
 	return None
 
 def master_tweet_processor(comm, sal_dict, data_url) -> None:
 	rank = comm.Get_rank()
 	size = comm.Get_size()
-	master_work = []
 	gcc_count = {'1gsyd': 0, '2gmel': 0, '3gbri': 0, '4gade': 0, '5gper': 0,'6ghob': 0, '7gdar': 0, '8acte': 0, '9oter': 0}
 	user_record = {}
-
-	# Read fiel object by object
-	with open(data_url, 'rb') as f:
-		for record in ijson.items(f, 'item', multiple_values=True):
-			user_id = record['data']['author_id']
-			location = record['includes']['places'][0]['full_name']
-			if size == 1:
+	
+	if size == 1:
+		with open(data_url, 'rb') as f:
+			for record in ijson.items(f, 'item', multiple_values=True):
+				user_id = record['data']['author_id']
+				location = record['includes']['places'][0]['full_name']
 				user_record, gcc_count = tweet.analyse((user_id, location), user_record, gcc_count, sal_dict)
-			else:
-				distribute = int(user_id)%(size-1) + 1
-				req = comm.send((user_id, location), dest=distribute, tag=distribute)
-	# Master's own task
-	for index, t in enumerate(master_work):
-		user_record, gcc_count = tweet.analyse(t, user_record, gcc_count, sal_dict)
+
 	# Result from the master
 	task_1 = gcc_count
 	task_2 = utility.sort_tweet_count(user_record, 10)
 	task_3 = utility.sort_unique_gcc_count(user_record, 10)
 	# Collect and summerise the result from the slaves
 	if size > 1:
-		res = marshall_tweets(comm, task_1, task_2, task_3)
+		res = summerise_data(comm, task_1, task_2, task_3)
 		task_1 = res['task_1']
 		task_2 = res['task_2']
 		task_3 = res['task_3']
@@ -64,24 +57,28 @@ def master_tweet_processor(comm, sal_dict, data_url) -> None:
 	print("\n")
 	return None
 
-def slave_tweet_processor(comm, sal_dict):
+def slave_tweet_processor(comm, sal_dict, data_url):
 	rank = comm.Get_rank()
 	size = comm.Get_size()
 	gcc_count = {'1gsyd': 0, '2gmel': 0, '3gbri': 0, '4gade': 0, '5gper': 0,'6ghob': 0, '7gdar': 0, '8acte': 0, '9oter': 0}
 	user_record = {}
+	
+	with open(data_url, 'rb') as f:
+		for record in ijson.items(f, 'item', multiple_values=True):
+			user_id = record['data']['author_id']
+			location = record['includes']['places'][0]['full_name']
+			if int(user_id) % (size-1) + 1 == rank:
+				user_record, gcc_count = tweet.analyse((user_id, location), user_record, gcc_count, sal_dict)
 
 	while True:
 		in_comm = comm.recv(source=0, tag=rank)
-		if isinstance(in_comm, tuple):
-			# Receive task from the master
-			user_record, gcc_count = tweet.analyse(in_comm, user_record, gcc_count, sal_dict)
-		elif isinstance(in_comm, str):
+		if isinstance(in_comm, str):
 			if in_comm in ("return_data"):
 				comm.send({'gcc_count': gcc_count, 'tweet_count': utility.sort_tweet_count(user_record, 10), 'unique_gcc_count': utility.sort_unique_gcc_count(user_record, 10)}, dest=0, tag=0)
 			elif in_comm in ("exit"):
 				exit(0)
 
-def marshall_tweets(comm, master_gcc_count, master_tweet_count, master_unique_gcc_count) -> ({}, {}, {}):
+def summerise_data(comm, master_gcc_count, master_tweet_count, master_unique_gcc_count) -> ({}, {}, {}):
 	processes = comm.Get_size()
 	total_gcc_count = [master_gcc_count]
 	total_tweet_count = master_tweet_count
